386ffa878aebdd70bb5894a11827bf28609fe9da
Eduardo Melo <eduardo.melo.braga@gmail.com>
2021-01-27 22:41:41 -0300

    feat - created repository structure and service layer

--- MicroservicesBuildingBlocksLibrary/src/repositories/mongodbRepository.js
+++ MicroservicesBuildingBlocksLibrary/src/repositories/mongodbRepository.js
@@ -4,12 +4,18 @@ export class MongoDBRepository {
     }
 
     findByField(fieldKey, value) {
-        const query = ({[fieldKey]: value});
+        const query = {[fieldKey]: value};
         return this.collection.findOne(query);
     }
 
-    findInRange(fieldKey, start, end) {
-
+    findInRange(fieldKey, lowerBondary, upperBoundary) {
+        const query = {
+            [fieldKey]: {
+                $gte: upperBoundary,
+                $lt: lowerBondary
+            }
+        };
+        return this.collection.find(query);
     }
 
     add(entity) {
@@ -17,6 +23,7 @@ export class MongoDBRepository {
     }
 
     addRange(entities) {
-
+        const jsonEntities = entities.map(entity => entity.toJSON());
+        this.collection.insertMany(jsonEntities);
     }
 }
